d313fe02b7a457ff9114e92dce6e31b389d7fe00
Eric Fiorani <efiorani305@gmail.com>
2018-05-01 18:23:54 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -56,7 +56,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
-programmer_hash.map {|vs| vs.each {|v| "#{v}"}}[:dennis_ritchie][:languages]
+programmer_hash[:dennis_ritchie][:languages]
 end
 
 def adding_matz
